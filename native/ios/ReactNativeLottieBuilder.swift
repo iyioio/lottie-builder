7b53a1a885f4a0fc8e810dc1a86a48fed47783e1
Scott Vance <logic@newbaselogic.com>
2021-12-18 09:16:55 -0500

    - re-init native

---
+++ native/ios/ReactNativeLottieBuilder.swift
@@ -0,0 +1,8 @@
+@objc(ReactNativeLottieBuilder)
+class ReactNativeLottieBuilder: NSObject {
+
+    @objc(multiply:withB:withResolver:withRejecter:)
+    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a*b)
+    }
+}

95b5c05a5b16dbc67c59f9854955eee140877784
Scott Vance <logic@newbaselogic.com>
2021-12-18 09:09:17 -0500

    - restart on native

--- native/example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  ReactNativeLottieBuilderExample
-//
-
-import Foundation
--- native/ios/ReactNativeLottieBuilder.h
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface ReactNativeLottieBuilder : NSObject <RCTBridgeModule>
-
-@end

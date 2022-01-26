253f2a5b7bf6ed0cf5e97319a9ecd3e279a66342
Scott Vance <logic@newbaselogic.com>
2022-01-26 14:27:13 -0500

    - A script to open xcode projects

---
+++ scripts/OpenAppXCode.ps1
@@ -0,0 +1,3 @@
+#!/usr/local/bin/pwsh
+open "$PSScriptRoot/../native/ios/ReactNativeLottieBuilder.xcodeproj/"
+open "$PSScriptRoot/../native/example/ios/ReactNativeLottieBuilderExample.xcworkspace/"
